cff6864449df35b1c04ca33d82e0ee6902894475
yogeshverma2 <yogesh.kumarverma@farmart.co>
2024-03-31 19:11:54 +0530

    wip

--- src/pages/homeScreen/MarketInsightsDisplay.jsx
+++ src/pages/homeScreen/MarketInsightsDisplay.jsx
@@ -8,7 +8,7 @@ const MarketInsightsDisplay = ({ selectedCrop, selectedMandi }) => {
   const [marketInsights, setMarketInsights] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://harmonious-horn-aa3f1ee25a.strapiapp.com/api/market-insights?filters[crop_id][id]=${selectedCrop}&filters[mandi_id][id]=${selectedMandi}`,{headers:{Authorization :'Bearer faeff3675b0b3589e13987c2a796dbabc956d05253e200472db4a68f53a63c059174932fa4cb3368a729dc7f3c8c8e01068cefd1f1f505fcf8566a8e15bb0bb0a797ecc93b778c41abd4d4157ad0fec6af1e764fc8bf638eca214c8f8bbbc7bb2c3649df7caeaba40f08da9f03b9005d796bcf1483b59e7767fbac4bc4e78b05'}})
+    axios.get(`https://harmonious-horn-aa3f1ee25a.strapiapp.com/api/market-insights?filters[crop_id][id]=${selectedCrop}&filters[mandi_id][id]=${selectedMandi}&populate=*`,{headers:{Authorization :'Bearer faeff3675b0b3589e13987c2a796dbabc956d05253e200472db4a68f53a63c059174932fa4cb3368a729dc7f3c8c8e01068cefd1f1f505fcf8566a8e15bb0bb0a797ecc93b778c41abd4d4157ad0fec6af1e764fc8bf638eca214c8f8bbbc7bb2c3649df7caeaba40f08da9f03b9005d796bcf1483b59e7767fbac4bc4e78b05'}})
       .then(response => {
         setMarketInsights(response.data.data);
       })
